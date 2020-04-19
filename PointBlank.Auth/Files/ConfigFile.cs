986ec897116709f47808a16e9361c410f46cf589
Gabriel Trucollo <gabriel.zaia@unoesc.edu.br>
2020-04-19 12:26:56 -0300

    Adicionado opção do arquivo de configuração

---
+++ PointBlank.Auth/Files/ConfigFile.cs
@@ -0,0 +1,66 @@
+﻿namespace PointBlank.Auth.Files
+{
+    using System;
+    using System.IO;
+    using OR.Base;
+
+    /// <summary>
+    /// Regras de negócio para gerenciamento da classe <see cref='ConfigFile'/>
+    /// </summary>
+    public class ConfigFile : BaseTextFile
+    {
+        #region Construtor
+        /// <summary>
+        /// Inicia uma nova instancia da classe <see cref="ConfigFile"/>
+        /// </summary>
+        public ConfigFile()
+            : base(@"Config/Configuracao.txt")
+        {
+        }
+        #endregion
+
+        #region Propriedades
+        /// <summary>
+        /// Obtém ou define NetworkHost
+        /// </summary>
+        public string NetworkHost
+        {
+            get
+            {
+                return this.ObterValorArquivo(nameof(NetworkHost));
+            }
+        }
+
+        /// <summary>
+        /// Obtém ou define NetworkPort
+        /// </summary>
+        public int NetworkPort
+        {
+            get
+            {
+                try
+                {
+                    return Convert.ToInt32(this.ObterValorArquivo(nameof(NetworkPort)));
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Informações padrões (Executado após a criação do arquivo)
+        /// </summary>
+        /// <param name="writer">Writer do arquivo gerado</param>
+        protected override void NovoArquivoInformacaoPadraoPartial(StreamWriter writer)
+        {
+            writer.WriteLine("; Configurações do Game Server");
+            writer.WriteLine("NetworkHost = 127.0.0.1");
+            writer.WriteLine("NetworkPort = 39191");
+        }
+        #endregion
+    }
+}
