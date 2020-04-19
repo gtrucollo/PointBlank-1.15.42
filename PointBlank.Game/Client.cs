be0d15e82048b076ac390cdbe616e6c4ec6d4e3f
Gabriel Trucollo <gabriel.zaia@unoesc.edu.br>
2020-04-19 12:16:01 -0300

    Alterações Diversas

---
+++ PointBlank.Game/Client.cs
@@ -0,0 +1,30 @@
+﻿namespace PointBlank.Game
+{
+    using System.Net.Sockets;
+    using OR.Base;
+    using OR.Library.Exceptions;
+
+    public class Client : BaseClient
+    {
+        #region Construtor
+        /// <summary>
+        /// Inicia uma nova instancia da classe <see cref="Client"/>
+        /// </summary>
+        /// <param name="tcpClient">Sessão criada para a conexão</param>
+        /// <param name="sessionSeed">Session seed da conexao</param>
+        /// <param name="showHex">Se true indica que é para mostrar os hex recebidos</param>
+        public Client(TcpClient tcpClient, uint sessionId, bool showHex)
+            : base(tcpClient, sessionId, showHex)
+        {
+        }
+        #endregion
+
+        #region Métodos
+        /// <inheritdoc/>
+        protected override void RunPacket(byte[] buffer)
+        {
+            throw new PointBlankException("Opção em desenvolvimento!");
+        }
+        #endregion
+    }
+}
